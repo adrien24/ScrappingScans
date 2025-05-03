74862da96aa5c353aa041421e4d3dfc43c533163
Adrien <adrien.bouteiller01@gmail.com>
2025-05-03 10:00:13 +0200

    test

--- supabase/functions/ma-fonction/index.ts
+++ supabase/functions/ma-fonction/index.ts
@@ -1,10 +1,24 @@
 // supabase/functions/ma-fonction/index.ts
 
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
+import { DOMParser } from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts'
+
+const url = 'https://example.com'
 
 serve(async (req) => {
   // Ta logique ici
-  console.log('Tâche CRON exécutée')
+  console.log('Hello from Supabase Function!')
+  console.log('Request URL:', req.url)
 
-  return new Response('OK', { status: 200 })
+  try {
+    const res = await fetch(url)
+    const html = await res.text()
+    const document: any = new DOMParser().parseFromString(html, 'text/html')
+
+    const pageHeader = document.querySelector('h1').textContent
+
+    console.log(pageHeader)
+  } catch (error) {
+    console.log(error)
+  }
 })

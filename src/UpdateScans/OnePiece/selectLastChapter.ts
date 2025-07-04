ed521ec64847218b5a37efa74140bf5bf93540b0
Adrien <adrien.bouteiller01@gmail.com>
2025-07-04 11:31:18 +0200

    :eyeglasses: fix error scrapping

--- src/UpdateScans/OnePiece/selectLastChapter.ts
+++ src/UpdateScans/OnePiece/selectLastChapter.ts
@@ -1,40 +1,38 @@
-import puppeteer from "puppeteer";
-import dotenv from "dotenv";
-dotenv.config();
+import puppeteer from 'puppeteer'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const { URL_ONEPIECE } = process.env;
-if (!URL_ONEPIECE) {
-  throw new Error("URL_ONEPICE is not defined in the environment variables");
-}
+// const { URL_ONEPIECE } = https://onepiecescan.fr/;
+// if (!URL_ONEPIECE) {
+//   throw new Error("URL_ONEPICE is not defined in the environment variables");
+// }
 
 export const selectLastChapter = async (): Promise<number> => {
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      executablePath:
-        "/home/hangover/.cache/puppeteer/chrome/linux-135.0.7049.114/chrome-linux64/chrome",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    const page = await browser.newPage();
+      headless: false,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+    const page = await browser.newPage()
 
-    await page.goto(URL_ONEPIECE, {
-      waitUntil: "networkidle2",
-    });
+    await page.goto('https://onepiecescan.fr/', {
+      waitUntil: 'networkidle2',
+    })
 
-    const firstChapter = await page.$eval("#All_chapters ul li ul li", (el) =>
+    const firstChapter = await page.$eval('#All_chapters ul li ul li', (el) =>
       el.textContent?.trim(),
-    );
+    )
 
-    const input = firstChapter || "";
-    const onlyNumbers = input.replace(/\D/g, "");
+    const input = firstChapter || ''
+    const onlyNumbers = input.replace(/\D/g, '')
 
-    const toNumber = parseInt(onlyNumbers, 10);
+    const toNumber = parseInt(onlyNumbers, 10)
 
-    await browser.close();
+    await browser.close()
 
-    return toNumber;
+    return toNumber
   } catch (err) {
-    console.log(err);
-    return 0;
+    console.log(err)
+    return 0
   }
-};
+}
